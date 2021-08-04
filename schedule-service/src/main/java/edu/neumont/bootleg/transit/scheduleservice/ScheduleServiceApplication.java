402503a9617fb4cc9d186b11dc6aca46ff24a715
Simeon Widdis <sawiddis@gmail.com>
2021-08-04 08:32:40 -0600

    Add Scheduling Service

---
+++ schedule-service/src/main/java/edu/neumont/bootleg/transit/scheduleservice/ScheduleServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.neumont.bootleg.transit.scheduleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleServiceApplication.class, args);
+	}
+
+}
