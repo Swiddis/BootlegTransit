b3500d940f19a7d7bb98f528b28ae236392725d1
Travja <the.only.t.craft@gmail.com>
2021-08-04 09:52:01 -0600

    Use Mysql

--- stops-service/src/main/java/edu/neumont/bootleg/transit/stopsservice/StopsServiceApplication.java
+++ stops-service/src/main/java/edu/neumont/bootleg/transit/stopsservice/StopsServiceApplication.java
@@ -2,8 +2,14 @@ package edu.neumont.bootleg.transit.stopsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "edu.neumont.bootleg.transit")
+@ComponentScan(basePackages = "edu.neumont.bootleg.transit")
+@EnableJpaRepositories(basePackages = "edu.neumont.bootleg.transit")
 public class StopsServiceApplication {
 
 	public static void main(String[] args) {
--- vehicle-service/src/main/java/edu/neumont/bootleg/transit/vehicleservice/VehicleServiceApplication.java
+++ vehicle-service/src/main/java/edu/neumont/bootleg/transit/vehicleservice/VehicleServiceApplication.java
@@ -2,8 +2,14 @@ package edu.neumont.bootleg.transit.vehicleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "edu.neumont.bootleg.transit")
+@ComponentScan(basePackages = "edu.neumont.bootleg.transit")
+@EnableJpaRepositories(basePackages = "edu.neumont.bootleg.transit")
 public class VehicleServiceApplication {
 
     public static void main(String[] args) {
