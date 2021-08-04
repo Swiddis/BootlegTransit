28008b5aa37f9d7be0337ff448b8136e2b994922
Simeon Widdis <sawiddis@gmail.com>
2021-08-04 08:37:12 -0600

    Add User Data Service

---
+++ user-service/src/main/java/edu/neumont/bootleg/transit/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.neumont.bootleg.transit.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/edu/neumont/bootleg/transit/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neumont.bootleg.transit.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
