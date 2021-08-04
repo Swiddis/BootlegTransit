fb8a1bd393e3bcf571e7adb4c61515f993b24362
Simeon Widdis <sawiddis@gmail.com>
2021-08-04 08:20:32 -0600

    Add Eureka Server Project

---
+++ eureka-server/src/main/java/edu/neumont/bootleg/transit/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package edu.neumont.bootleg.transit.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
---
+++ eureka-server/src/test/java/edu/neumont/bootleg/transit/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neumont.bootleg.transit.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
