06e16f75220311ac0c429956093359c1e5857e9e
Simeon Widdis <sawiddis@gmail.com>
2021-08-04 08:30:44 -0600

    Add Stops Service

---
+++ stops-service/src/main/java/edu/neumont/bootleg/transit/stopsservice/StopsServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.neumont.bootleg.transit.stopsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StopsServiceApplication.class, args);
+	}
+
+}
---
+++ stops-service/src/test/java/edu/neumont/bootleg/transit/stopsservice/StopsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neumont.bootleg.transit.stopsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StopsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
