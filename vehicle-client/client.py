11d427403fe1af0fbbae3fc8c3c2a3d647a46c92
Simeon Widdis <sawiddis@gmail.com>
2021-08-10 08:17:44 -0600

    Add vehicle client with random movement

---
+++ vehicle-client/client.py
@@ -0,0 +1,62 @@
+import requests
+import time
+import random
+import math
+import json
+
+def main():
+    VEHICLE_COUNT = 5
+
+    vehicles = [create_vehicle() for _ in range(VEHICLE_COUNT)]
+
+    while True:
+        time.sleep(random.random())
+        idx = random.randint(0, len(vehicles) - 1)
+        vehicles[idx] = update_vehicle(vehicles[idx])
+
+# Random point chosen uniformly in an ellipse of given width and height
+def random_ellipse(width, height=None):
+    height = width if height is None else height
+    phi = 2.0 * math.pi * random.random()
+    rho = math.sqrt(random.random())
+    x, y = rho * math.cos(phi), rho * math.sin(phi)
+    return (x * width * 0.5, y * height * 0.5)
+
+def create_vehicle():
+    vehicle = {
+        "name": "Bus" + str(random.randint(10**8, 10**9-1)),
+        "routeId": 0,
+        "routeIdx": 0,
+        "lat": 0.0,
+        "lng": 0.0
+    }
+
+    post_req = requests.post(
+        "http://localhost:8070/vehicle-service/vehicle",
+        headers={"Content-Type": "application/json"},
+        data = json.dumps(vehicle)
+    )
+    if post_req.status_code != 200:
+        raise ConnectionError("Failed to create vehicle: " + post_req.text)
+    print('Created Vehicle:', post_req.text)
+    return json.loads(post_req.text)
+
+def update_vehicle(vehicle):
+    update_lat, update_lng = random_ellipse(1)
+    updates = {
+        "lat": round(vehicle["lat"] + update_lat, 4),
+        "lng": round(vehicle["lng"] + update_lng, 4)
+    }
+
+    patch_req = requests.patch(
+        f"http://localhost:8070/vehicle-service/vehicle/{vehicle['id']}",
+        headers={"Content-Type": "application/json"},
+        data = json.dumps(updates)
+    )
+    if patch_req.status_code != 200:
+        raise ConnectionError("Failed to update vehicle: " + patch_req.text)
+    print('Updated Vehicle:', patch_req.text)
+    return json.loads(patch_req.text)
+
+if __name__ == "__main__":
+    main()
