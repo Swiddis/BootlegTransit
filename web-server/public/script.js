7331cc89e316a82b8e37f1b687fa00d85f1d5c87
Simeon Widdis <sawiddis@gmail.com>
2021-08-11 09:14:06 -0600

    Better encapsulate vehicle rendering

--- web-server/public/script.js
+++ web-server/public/script.js
@@ -21,6 +21,23 @@ const verify_login = () => {
     return true;
 }
 
+const render_vehicles = (vehicles) => {
+    let canvas = document.getElementById("canvas");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#0a2239";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    console.log([canvas.width, canvas.height]);
+    let dims = [canvas.height / 2, canvas.width / 2];
+    for (let vehicle of vehicles) {
+        coord = [vehicle.lat * (dims[0] / 180) + dims[0], vehicle.lng * (dims[1] / 90) + dims[1]]
+        console.log(coord);
+        ctx.beginPath();
+        ctx.fillStyle = "#f28f3b";
+        ctx.arc(coord[0], coord[1], 5, 0, 260);
+        ctx.fill();
+    }
+}
+
 const vehicle_track = () => {
     fetch(
         "http://localhost:8070/vehicle-service/vehicle"
@@ -29,20 +46,7 @@ const vehicle_track = () => {
     ).then(
         vehicles => {
             console.log(vehicles);
-            let canvas = document.getElementById("canvas");
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.fillStyle = "#0a2239";
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-            console.log([canvas.width, canvas.height]);
-            let dims = [canvas.height / 2, canvas.width / 2];
-            for (let vehicle of vehicles) {
-                coord = [vehicle.lat * (dims[0] / 180) + dims[0], vehicle.lng * (dims[1] / 90) + dims[1]]
-                console.log(coord);
-                ctx.beginPath();
-                ctx.fillStyle = "#f28f3b";
-                ctx.arc(coord[0], coord[1], 5, 0, 260);
-                ctx.fill();
-            }
+            render_vehicles(vehicles);
         }
     ).catch(
         err => {
