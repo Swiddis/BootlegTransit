e04f51a274f459907812c8d19bd9e99882713fb2
Travja <the.only.t.craft@gmail.com>
2021-08-12 11:46:58 -0600

    Security actually works

--- cloud-gateway/src/main/java/edu/neumont/bootleg/transit/cloudgateway/SecurityConfiguration.java
+++ cloud-gateway/src/main/java/edu/neumont/bootleg/transit/cloudgateway/SecurityConfiguration.java
@@ -1,28 +1,30 @@
 package edu.neumont.bootleg.transit.cloudgateway;
 
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
 @Configuration
-//@EnableWebSecurity
 @EnableWebFluxSecurity
-//@AutoConfigureAfter({ReactiveSecurityAutoConfiguration.class})
-public class SecurityConfiguration/* extends WebSecurityConfigurerAdapter*/ {
+public class SecurityConfiguration {
 
-//    @Bean
-//    public WebFilterChainProxy webFilterChainProxy() {
-//        return new WebFilterChainProxy();
-//    }
+    private final BCryptPasswordEncoder bcryptEncoder;
+
+    public SecurityConfiguration(BCryptPasswordEncoder bcryptEncoder) {
+        this.bcryptEncoder = bcryptEncoder;
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder bcryptEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public MapReactiveUserDetailsService userDetailsService() {
@@ -30,46 +32,28 @@ public class SecurityConfiguration/* extends WebSecurityConfigurerAdapter*/ {
                 .withUsername("user")
                 .password("password")
                 .roles("USER")
+                .passwordEncoder(bcryptEncoder::encode)
                 .build();
         return new MapReactiveUserDetailsService(user);
     }
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
-        httpSecurity.csrf().disable()
-                .authorizeExchange()
+        httpSecurity.csrf().disable();
+
+        httpSecurity.authorizeExchange()
+                .pathMatchers("/*/actuator/**")
+                .permitAll()
+                .pathMatchers(HttpMethod.GET)
+                .permitAll();
+
+        httpSecurity.authorizeExchange()
                 .anyExchange()
-                .authenticated()
+                .hasRole("USER")
                 .and()
                 .httpBasic();
 
-        System.out.println(" ----------------- HEY WE DID IT -----------------------");
-//
-//        httpSecurity.securityMatcher(ServerWebExchangeMatchers.pathMatchers("/actuator/**"))
-//                .authorizeExchange()
-//                .pathMatchers("/actuator/**")
-//                .hasRole("ACTUATOR_ADMIN_ROLE");
-//
-//        httpSecurity.authorizeExchange()
-//                .anyExchange().hasRole("USER")
-//                .and().httpBasic();
-
         return httpSecurity.build();
     }
-
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().withUser("user").password("password")
-//                .roles("USER").and().withUser("admin").password("admin")
-//                .roles("ADMIN");
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable().authorizeRequests()
-//                .anyRequest().authenticated()
-//                .and().httpBasic();
-//        http.build();
-//    }
 
 }
