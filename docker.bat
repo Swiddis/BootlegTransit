0b5e51ca229c46ead9d921050e22feff8935a362
Simeon Widdis <sawiddis@gmail.com>
2021-08-16 20:59:03 -0600

    Reformat docker.bat

--- docker.bat
+++ docker.bat
@@ -1,3 +1,6 @@
-CALL mvn package -f build-all -DskipTests && docker-compose up --build & docker-compose down & docker image prune -f
+CALL mvn package -f build-all -DskipTests
+CALL docker-compose up --build
+CALL docker-compose down
+CALL docker image prune -f
 
-pause
+PAUSE
